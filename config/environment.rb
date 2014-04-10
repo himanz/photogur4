f124d4aa5bfc40dbb04dd2ffe2e925f44d6d94d4
Jonathan Chang <jonoschang7@gmail.com>
2014-04-10 12:54:18 -0400

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Photogur4::Application.initialize!
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Photogur4::Application.config.session_store :cookie_store, key: '_photogur4_session'
